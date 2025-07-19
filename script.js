a90580bdf6b21a020091ff25db4b4ed9ef18a5b5
ProHubCore <prohubmain@gmail.com>
2025-07-19 17:12:08 +0300

    Появление анимация с JS

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.getElementById("changeTextBtn").addEventListener("click", function () {
-  document.getElementById("demoText").textContent = "Текст изменён!";
-});
-
-document.getElementById("greetForm").addEventListener("submit", function (e) {
-  e.preventDefault(); // отменяем перезагрузку
-  const name = document.getElementById("nameInput").value;
-  document.getElementById("greetingText").textContent = `Привет, ${name}!`;
-});
+ document.addEventListener("DOMContentLoaded", function() {
+      document.querySelector('.block').classList.add('show');
+    });
